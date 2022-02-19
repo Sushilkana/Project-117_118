58342f4e5925785fed470936be03bdda488fa348
Sushilkana <86962755+Sushilkana@users.noreply.github.com>
2022-02-20 01:29:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+array_1=["pen","paper","book","bottle"];
+random_no = Math.floor((Math.random()*array_1.length)+1);
+Element_of_array = array_1[random_no];
+var random_number = 0;
+var timer_counter = 0;
+var timer_check = "";
+var drawn_sketch = "";
+var answer_holder = "";
+var score = 0;
+
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(900,500);
+    background("white");
+    canvas.position(300,400);
+}
+
+function draw(){
+
+}
+
