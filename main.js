66e1a5f306448a375952a838ebf75b161c4b3960
Sushilkana <86962755+Sushilkana@users.noreply.github.com>
2022-02-26 12:00:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
-array_1=["pen","paper","book","bottle"];
-random_no = Math.floor((Math.random()*array_1.length)+1);
-Element_of_array = array_1[random_no];
-var random_number = 0;
+array_1 = ["pen","paper","book","bottle","car","Pizza","School"];
+random_numbers = Math.floor((Math.random()*array_1.length));
+Element_of_array = array_1[random_numbers];
+console.log("Sketch to be drawn: " + Element_of_array);
+
 var timer_counter = 0;
 var timer_check = "";
 var drawn_sketch = "";
 var answer_holder = "";
 var score = 0;
+
+document.getElementById("p3").innerHTML = "Sketch to be drawn: " + Element_of_array;
+
 
 function preload(){
 
@@ -19,6 +23,26 @@ function setup(){
 }
 
 function draw(){
-
+    Check_Sketch();
 }
 
+function Check_Sketch(){
+    timer_counter = timer_counter + 1;
+    document.getElementById("Timer_span").innerHTML = "Timer" + timer_counter;
+    if (timer_counter >= 600){
+        timer_counter = 0;
+        timer_counter = "conpleted  1";
+    }
+
+    if (timer_counter == "Completed"){
+        timer_counter = 0;
+        answer_holder = "set";
+        timer_check = 0;
+        answer_holder = "";
+        UpdateCanvas();
+    }
+}
+
+function UpdateCanvas(){
+    canvas.background("red")
+}
